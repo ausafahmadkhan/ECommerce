72246c17868a463f29e4ec308590a9980a993ed8
ausafkhan <khan.aakhan.ausaf7@gmail.com>
2019-08-24 14:47:38 +0530

    Added log implementation

--- App/src/main/java/com/example/Shopping/App.java
+++ App/src/main/java/com/example/Shopping/App.java
@@ -1,5 +1,7 @@
 package com.example.Shopping;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,8 @@ public class App
 {
     public static void main(String args[])
     {
+        Logger logger = LogManager.getLogger(App.class);
+        logger.info("App started");
         SpringApplication.run(App.class, args);
     }
 }
